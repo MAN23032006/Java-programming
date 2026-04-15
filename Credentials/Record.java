c8f2b8c3459f260aef9d4398371fe552e4dba0bb
Manik Sharma <2024a6r002@mietjammu.in>
2026-04-15 12:34:55 +0530

    Add files via upload

---
+++ Credentials/Record.java
@@ -0,0 +1,17 @@
+// package credentails in java
+package credentials;
+public class Record{
+    publuic String Username(){
+        return "User01";
+    }
+    public String Pass(){
+        return "Pass01";
+
+    }
+}
+
+
+
+
+
+
